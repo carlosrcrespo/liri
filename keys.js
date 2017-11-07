56b2cb8a0358a42ecb5dc823e766f344b3aa897d
carlos crespo <30513554+carlosrcrespo@users.noreply.github.com>
2017-11-06 22:33:32 -0500

    first commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'heoh1XnKgOhIMG6LbZeCOjU3b',
+  consumer_secret: 'zmu3FlEPAfcHICIpvI0xrYNvkyKrssEgS2g4VhC7862YyIIbyD',
+  access_token_key: '926663152569286656-IgswphMGjp2qMakDdA9v2mZmkTYO3YE',
+  access_token_secret: 'TBwSHMSp9HbHuIAC0mzg22CjZlOmDhZ8UA6dR2gpZGrrC',
+}
+
+module.exports = twitterKeys;
